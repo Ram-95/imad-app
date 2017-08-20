7160b72771819ec71bfda1ef66ae7bff8ad1ac47
Ram Babu <ram.kottapally@gmail.com>
2017-08-20 16:28:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,24 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 //alert('Hi! Welcome to IMAD.');
+var button = document.getElementById('counter');
+button.onclick = function() {
+// Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function()
+    {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if(request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }  
+    };
+    
+    //Make the request
+    request.open('GET', 'http://ramkottapally.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
