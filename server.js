eabd21c880df91717658722eb88bc902318b37c3
Ram Babu <ram.kottapally@gmail.com>
2017-08-20 16:05:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req, res) {
+   counter = counter + 1;
+   res.send(counter.toString()); 
+});
+
 
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
@@ -102,11 +108,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-var counter = 0;
-app.get('/counter', function(req, res){
-   counter = counter+1;
-   res.send(counter.toString()); 
-});
+
 
 
 app.get('/ui/style.css', function (req, res) {
