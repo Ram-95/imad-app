316f71025cffac56762d204e89de7b754f6748fd
Ram Babu <ram.kottapally@gmail.com>
2017-08-31 18:53:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 /* Creating the configuration for DB */
 var config = {
@@ -30,7 +33,10 @@ app.get('/hash/:input',function(req, res) {
 });
 
 
-app.get('/create-user', function(req, res) {
+app.post('/create-user', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    
    var salt = crypto.getRandomBytes(128).toString('hex');
    var dbString = hash(password, salt);
    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
