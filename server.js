1dfae364a4c6dafc8780641c32d7af6bbc92ac2e
Ram Babu <ram.kottapally@gmail.com>
2017-08-31 18:47:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,6 +30,20 @@ app.get('/hash/:input',function(req, res) {
 });
 
 
+app.get('/create-user', function(req, res) {
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result.rows));
+       }
+   });
+    
+});
+
+
 
 /* Creating Articles objects to load necessary articles */
 // This piece of code is not necessary as we are loading the articles from the DB below. This section can be removed.
